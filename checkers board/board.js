1027169aad0e339ecd613925dae3a83b90eba0ee
nicholas ayim <99901352+Nicholas-Ayim@users.noreply.github.com>
2023-06-23 17:09:13 +0000

    Add files via upload
    this a checker that normally termed as 'dammy' in Africa continent specially Ghana. an interface that contains some basic functionalities like swapping, moving and crowning

---
+++ checkers board/board.js
@@ -0,0 +1,59 @@
+
+let board = document.querySelector('.board');
+let sourceCellIndex = -1; // Variable to store the index of the source cell
+
+for (let i = 0; i < 64; i++) {
+  const cell = document.createElement('div');
+  cell.classList.add('cell');
+  const imageContainer = document.createElement('div');
+
+  if ((Math.floor(i / 8) % 2 === i % 2) && i < 24) {
+    cell.classList.add('black');
+    const imageElement = document.createElement('img');
+    imageElement.src = 'checker2.jpg';
+    imageElement.classList.add('piece2');
+    imageContainer.appendChild(imageElement);
+    cell.appendChild(imageContainer);
+  } else if ((Math.floor(i / 8) % 2 === i % 2) && i > 40) {
+    cell.classList.add('blue');
+
+    const image = document.createElement('img');
+    image.src = 'checke3.jpg';
+    image.classList.add('piece1');
+
+    imageContainer.appendChild(image);
+    cell.appendChild(imageContainer);
+  } else if (((Math.floor(i / 8)) % 2 === i % 2) && i > 23 && i < 32) {
+    cell.classList.add('black');
+  } else if (((Math.floor(i / 8)) % 2 === i % 2) && i > 31 && i < 39) {
+    cell.classList.add('blue');
+  }
+  if((i === 9) || (i === 16)){
+    cell.innerHTML = '';
+  }
+  if (i === 0) {
+    imageContainer.addEventListener('dragstart', function(event) {
+      sourceCellIndex = i; // Set the source cell index
+      event.dataTransfer.setData('text/plain', 'checker2.jpg');
+    });
+  } else if ((i === 9) || (i === 16)){
+    
+    cell.addEventListener('dragover', function(event) {
+      event.preventDefault();
+    });
+
+    cell.addEventListener('drop', function(event) {
+      event.preventDefault();
+      const dropElementDom = document.querySelector('.board .cell:nth-child(1) .piece2');
+
+      if (cell.children.length === 0 && sourceCellIndex === 0) {
+        // Move the image element to the current cell
+        cell.appendChild(dropElementDom);
+      }
+
+      sourceCellIndex = -1; // Reset the source cell index
+    });
+  }
+
+  board.appendChild(cell);
+}
